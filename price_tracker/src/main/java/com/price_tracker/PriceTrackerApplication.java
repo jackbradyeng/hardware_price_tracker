126838bc0f3940d115c238b8015f8b367ec8266a
jackb <jackbradyx@proton.me>
2026-02-01 18:33:12 +1000

    Initial commit

---
+++ price_tracker/src/main/java/com/price_tracker/PriceTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.price_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceTrackerApplication.class, args);
+	}
+
+}
