eacd47869292067ba02c8acbc9c79248e2e3a120
jackb <jackbradyx@proton.me>
2026-02-02 19:29:44 +1000

    Add PostgreSQL connection

--- src/main/java/com/price_tracker/PriceTrackerApplication.java
+++ src/main/java/com/price_tracker/PriceTrackerApplication.java
@@ -1,10 +1,27 @@
 package com.price_tracker;
 
+import javax.sql.DataSource;
+import lombok.extern.java.Log;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
-public class PriceTrackerApplication {
+@Log
+public class PriceTrackerApplication implements CommandLineRunner {
+
+    public final DataSource dataSource;
+
+    // constructor
+    public PriceTrackerApplication(final DataSource dataSource) { this.dataSource = dataSource; }
+
+    @Override
+    public void run(final String... args) {
+        log.info("Datasource: " + dataSource.toString());
+        final JdbcTemplate restTemplate = new JdbcTemplate(dataSource);
+        restTemplate.execute("select 1");
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(PriceTrackerApplication.class, args);
