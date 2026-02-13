0aec686875efb5ed74b043857bcb0265fe2f3d4a
jackb <jackbradyx@proton.me>
2026-02-13 16:27:52 +1000

    Update testing data

--- src/test/java/com/price_tracker/repositories/TestDataUtility.java
+++ src/test/java/com/price_tracker/repositories/TestDataUtility.java
@@ -13,17 +13,13 @@ public class TestDataUtility {
     public GPU createTestGPU() {
         String modelNumber = "PRIME-RTX5070TI-O16G";
         String name = "Asus Prime GeForce RTX 5070 Ti OC 16G Graphics Card";
-        String chip = "NVIDIA";
-        String board = "ASUS";
-        int vram = 16;
+        String brand = "ASUS";
         BigDecimal price = new BigDecimal("1598.00");
 
         return GPU.builder()
                 .modelNumber(modelNumber)
                 .name(name)
-                .chipManufacturer(chip)
-                .boardManufacturer(board)
-                .videoMemory(vram)
+                .brand(brand)
                 .price(price)
                 .build();
     }
@@ -32,16 +28,12 @@ public class TestDataUtility {
         String id = "KF560C36BBE2K2-64";
         String name = "Kingston 64GB (2x32GB) Fury Beast CL36 6000MHz DDR5 RAM (KF560C36BBE2K2-64)";
         String brand = "Kingston";
-        int volume = 64;
-        int clockRate = 6000;
         BigDecimal price = new BigDecimal("1299.00");
 
         return RAM.builder()
                 .id(id)
                 .name(name)
                 .brand(brand)
-                .volume(volume)
-                .clockRate(clockRate)
                 .price(price)
                 .build();
     }
