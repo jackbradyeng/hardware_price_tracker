3271333b7d8fe399c9942f2cccf6a88c04bb4e99
jackb <jackbradyx@proton.me>
2026-02-14 13:15:40 +1000

    Update testing data

--- src/test/java/com/price_tracker/repositories/TestDataUtility.java
+++ src/test/java/com/price_tracker/repositories/TestDataUtility.java
@@ -1,40 +1,51 @@
 package com.price_tracker.repositories;
 
-import com.price_tracker.domain.entities.GPU;
-import com.price_tracker.domain.entities.RAM;
+import com.price_tracker.domain.entities.GPUEntity;
+import com.price_tracker.domain.entities.RAMEntity;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import java.math.BigDecimal;
 
 @Data
 @NoArgsConstructor
 public class TestDataUtility {
 
-    public GPU createTestGPU() {
+    public GPUEntity createTestGPU() {
         String modelNumber = "PRIME-RTX5070TI-O16G";
+        String chip = "RTX 5070Ti";
+        String chipManufacturer = "NVIDIA";
+        String boardManufacturer = "ASUS";
         String name = "Asus Prime GeForce RTX 5070 Ti OC 16G Graphics Card";
-        String brand = "ASUS";
-        BigDecimal price = new BigDecimal("1598.00");
 
-        return GPU.builder()
+        return GPUEntity.builder()
                 .modelNumber(modelNumber)
+                .chip(chip)
+                .chipManufacturer(chipManufacturer)
                 .name(name)
-                .brand(brand)
-                .price(price)
+                .boardManufacturer(boardManufacturer)
                 .build();
     }
 
-    public RAM createTestRAM() {
+    public RAMEntity createTestRAM() {
         String id = "KF560C36BBE2K2-64";
         String name = "Kingston 64GB (2x32GB) Fury Beast CL36 6000MHz DDR5 RAM (KF560C36BBE2K2-64)";
         String brand = "Kingston";
-        BigDecimal price = new BigDecimal("1299.00");
+        String standard = "DDR5";
+        String latency = "CL36";
+        int volume = 64;
+        int dimmCount = 2;
+        int clockRate = 6000;
+        Double voltage = 1.35;
 
-        return RAM.builder()
-                .id(id)
+        return RAMEntity.builder()
+                .modelNumber(id)
                 .name(name)
                 .brand(brand)
-                .price(price)
+                .standard(standard)
+                .latency(latency)
+                .volume(volume)
+                .dimmCount(dimmCount)
+                .clockRate(clockRate)
+                .voltage(voltage)
                 .build();
     }
 }
