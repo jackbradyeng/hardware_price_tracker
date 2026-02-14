3a28f5f5d545808b02fd633828ac40ae8d1d2e85
jackb <jackbradyx@proton.me>
2026-02-14 13:10:35 +1000

    Update DB table names

--- src/main/java/com/price_tracker/constants/DBTableNames.java
+++ src/main/java/com/price_tracker/constants/DBTableNames.java
@@ -3,5 +3,7 @@ package com.price_tracker.constants;
 public final class DBTableNames {
 
     public static final String GPU_TABLE_NAME = "GPUs";
+    public static final String GPU_PRICE_HISTORY = "GPU_PRICE_HISTORY";
     public static final String RAM_TABLE_NAME = "RAM";
+    public static final String RAM_PRICE_HISTORY = "RAM_PRICE_HISTORY";
 }
