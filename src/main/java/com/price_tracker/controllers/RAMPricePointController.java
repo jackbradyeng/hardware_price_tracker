451866365f2ef76ab0b2afc1f3bce69433a4ae18
jackb <jackbradyx@proton.me>
2026-02-16 17:47:41 +1000

    Add skeleton RAM price point controller

---
+++ src/main/java/com/price_tracker/controllers/RAMPricePointController.java
@@ -0,0 +1,12 @@
+package com.price_tracker.controllers;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.java.Log;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@Log
+public class RAMPricePointController {
+
+}
