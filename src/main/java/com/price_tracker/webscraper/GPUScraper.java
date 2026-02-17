8d5427b6304a362cae6148a54f72d1b3e4afc1f6
jackb <jackbradyx@proton.me>
2026-02-17 19:30:45 +1000

    Convert GPU scraper to interface

--- src/main/java/com/price_tracker/webscraper/GPUScraper.java
+++ src/main/java/com/price_tracker/webscraper/GPUScraper.java
@@ -1,47 +1,12 @@
 package com.price_tracker.webscraper;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import java.io.IOException;
+import com.price_tracker.domain.entities.GPUPricePoint;
 
-@Data
-@NoArgsConstructor
-public class GPUScraper {
+public interface GPUScraper {
 
-    public String scrapeGPUName(String url) {
+    String[] scrapeGPUData(String url);
 
-        // css locations for Umart GPU parameters
-        String umartCSSNameLocation = ".goods_title h1";
-        String umartCSSPriceLocation = "span.goods-price";
-        String umartCSSModelLocation = "li:contains(Model Number:)";
-        String umartCSSBrandLocation = "li:contains(Brand:)";
+    boolean validateScrapedData(String[] scrapedData);
 
-        try {
-            Document document = Jsoup.connect(url).get();
-            String name = document.select(umartCSSNameLocation).text();
-            String price = document.select(umartCSSPriceLocation).text();
-            String rawModelNumber = document.select(umartCSSModelLocation).text();
-            String rawBrand = document.select(umartCSSBrandLocation).text();
-
-            // if the return element has "Model Number: ", remove it.
-            String modelNumber = rawModelNumber.contains(":") ? rawModelNumber.split(":")[1].trim() : rawModelNumber;
-
-            // if the return element has "Brand: ", remote it.
-            String brand = rawBrand.contains(":") ? rawBrand.split(":")[1].trim() : rawBrand;
-
-            System.out.println("====================");
-            System.out.println("UMART - GPUs");
-            System.out.println(name);
-            System.out.println(price);
-            System.out.println(modelNumber);
-            System.out.println(brand);
-            System.out.println("====================");
-            return name;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
+    GPUPricePoint createGPUPricePoint(String[] scrapedData);
 }
