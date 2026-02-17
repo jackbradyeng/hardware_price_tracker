9fcce2f7e59d01d20c840534a3251834edfade2e
jackb <jackbradyx@proton.me>
2026-02-17 19:27:02 +1000

    Add currency & vendor constants

---
+++ src/main/java/com/price_tracker/constants/CurrencyConstants.java
@@ -0,0 +1,8 @@
+package com.price_tracker.constants;
+
+public class CurrencyConstants {
+
+    // currency types
+    public static final String AUD = "AUD";
+    public static final String USD = "USD";
+}
---
+++ src/main/java/com/price_tracker/constants/VendorConstants.java
@@ -0,0 +1,8 @@
+package com.price_tracker.constants;
+
+public class VendorConstants {
+
+    // vendor names
+    public static final String UMART = "UMART";
+    public static final String PC_CASE_GEAR = "PC CASE GEAR";
+}
