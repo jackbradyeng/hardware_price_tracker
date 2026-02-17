1053cc77c2c25a7bc653c831693c04a53624ca9b
jackb <jackbradyx@proton.me>
2026-02-17 19:28:09 +1000

    Add Umart GPU scraper

---
+++ src/main/java/com/price_tracker/constants/UmartCSSLocations.java
@@ -0,0 +1,8 @@
+package com.price_tracker.constants;
+
+public class UmartCSSLocations {
+
+    public static final String UMART_CSS_NAME_LOCATION = ".goods_title h1";
+    public static final String UMART_CSS_PRICE_LOCATION = "span.goods-price";
+    public static final String UMART_CSS_MODEL_LOCATION = "li:contains(Model Number:)";
+}
