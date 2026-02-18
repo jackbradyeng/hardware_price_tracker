4b15cbb490743e015e3586174144c6b7d71df57f
jackb <jackbradyx@proton.me>
2026-02-18 18:20:17 +1000

    Add vendor entity

--- src/main/java/com/price_tracker/constants/DatabaseTableNames.java
+++ src/main/java/com/price_tracker/constants/DatabaseTableNames.java
@@ -6,4 +6,5 @@ public final class DatabaseTableNames {
     public static final String GPU_PRICE_HISTORY = "GPU_PRICE_HISTORY";
     public static final String RAM_TABLE_NAME = "RAM";
     public static final String RAM_PRICE_HISTORY = "RAM_PRICE_HISTORY";
+    public static final String VENDOR_TABLE_NAME = "VENDORS";
 }
---
+++ src/main/java/com/price_tracker/domain/entities/VendorEntity.java
@@ -0,0 +1,26 @@
+package com.price_tracker.domain.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import static com.price_tracker.constants.DatabaseTableNames.VENDOR_TABLE_NAME;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = VENDOR_TABLE_NAME)
+public class VendorEntity {
+
+    @Id
+    private String vendor;
+    private String primaryLocation;
+    private String primaryCurrency;
+    private String homeURL;
+    private boolean activeStatus;
+}
