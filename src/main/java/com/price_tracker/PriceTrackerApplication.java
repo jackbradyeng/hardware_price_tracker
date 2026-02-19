666486566e2c4dae997b4524a1b9a630570af5c0
jackb <jackbradyx@proton.me>
2026-02-19 13:31:17 +1000

    Add enable scheduling

--- src/main/java/com/price_tracker/PriceTrackerApplication.java
+++ src/main/java/com/price_tracker/PriceTrackerApplication.java
@@ -3,8 +3,10 @@ package com.price_tracker;
 import lombok.extern.java.Log;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 @Log
 public class PriceTrackerApplication {
 
