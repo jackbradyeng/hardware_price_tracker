91fd15fbedf8ec00bfd3f9ece239d5307e3dd76a
jack-brady <jackbradyx@proton.me>
2026-02-20 16:52:34 +1000

    Convert CRUD to JPA repository

--- src/main/java/com/price_tracker/repositories/GPURepository.java
+++ src/main/java/com/price_tracker/repositories/GPURepository.java
@@ -1,9 +1,10 @@
 package com.price_tracker.repositories;
 
 import com.price_tracker.domain.entities.GPUEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GPURepository extends CrudRepository<GPUEntity, String> {
+public interface GPURepository extends JpaRepository<GPUEntity, String> {
 }
--- src/main/java/com/price_tracker/repositories/RAMRepository.java
+++ src/main/java/com/price_tracker/repositories/RAMRepository.java
@@ -1,9 +1,10 @@
 package com.price_tracker.repositories;
 
 import com.price_tracker.domain.entities.RAMEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RAMRepository extends CrudRepository<RAMEntity, String> {
+public interface RAMRepository extends JpaRepository<RAMEntity, String> {
 }
