bba934ed12d7c1b8e2a42e2329efe792d298c3e9
jack-brady <jackbradyx@proton.me>
2026-02-21 18:39:47 +1000

    Add Umart product skeleton controller

---
+++ src/main/java/com/price_tracker/controllers/UmartProductController.java
@@ -0,0 +1,12 @@
+package com.price_tracker.controllers;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Log
+public class UmartProductController {
+
+}
