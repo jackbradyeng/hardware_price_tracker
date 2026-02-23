52c664030c74da5dda59934b4a56a5dc109fa600
jack-brady <jackbradyx@proton.me>
2026-02-23 13:38:52 +1000

    Update web domain names

--- src/main/java/com/price_tracker/constants/WebDomainNames.java
+++ src/main/java/com/price_tracker/constants/WebDomainNames.java
@@ -9,7 +9,4 @@ public class WebDomainNames {
             "https://www.umart.com.au/product/asus-prime-geforce-rtx-5070-ti-oc-16g-graphics-card-prime-rtx5070ti-o16g-81687";
     public static final String UMART_KINGSTON_KINGSTON_F64G =
             "https://www.umart.com.au/product/kingston-64gb-2x32gb-fury-beast-cl36-6000mhz-ddr5-ram-kf560c36bbe2k2-64-86953";
-
-    /// PC CASE GEAR DOMAINS
-    public static final String PCCG_HOME = "https://www.pccasegear.com/";
 }
