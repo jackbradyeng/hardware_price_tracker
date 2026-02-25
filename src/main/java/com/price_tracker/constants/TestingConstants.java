618d319d472313e4506b7ccf8c8528c92e906da4
jack-brady <jackbradyx@proton.me>
2026-02-25 22:20:49 +1000

    Fix GPU scraping tests

---
+++ src/main/java/com/price_tracker/constants/TestingConstants.java
@@ -0,0 +1,7 @@
+package com.price_tracker.constants;
+
+public final class TestingConstants {
+
+    public static final String TESTING_GPU_MODEL_NUMBER = "PRIME-RTX5070TI-O16G";
+    public static final String TESTING_RAM_MODEL_NUMBER = "KF560C36BBE2K2-64";
+}
--- src/test/java/com/price_tracker/scrapers/GPUEntityScraperTests.java
+++ src/test/java/com/price_tracker/scrapers/GPUEntityScraperTests.java
@@ -4,7 +4,9 @@ import com.price_tracker.webscraper.product_services.impl.UmartGPUScraper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static com.price_tracker.constants.TestingConstants.TESTING_GPU_MODEL_NUMBER;
 import static com.price_tracker.constants.WebDomainNames.UMART_ASUS_5070TI;
 
 @SpringBootTest
@@ -13,11 +15,12 @@ public class GPUEntityScraperTests {
 
     private final UmartGPUScraper scraper;
 
-    public GPUEntityScraperTests() { this.scraper = new UmartGPUScraper(); }
+    @Autowired
+    public GPUEntityScraperTests(UmartGPUScraper scraper) { this.scraper = scraper; }
 
     @Test
     public void testThatUmartGPUScraperReturnsExpectedModelNumber() {
         String[] scrapedValues = scraper.scrapeProductData(UMART_ASUS_5070TI);
-        assert scrapedValues[0].equals("PRIME-RTX5070TI-O16G");
+        assert scrapedValues[0].equals(TESTING_GPU_MODEL_NUMBER);
     }
 }
