88d90f317de04a5f1a8df094c06297077f773d57
jack-brady <jackbradyx@proton.me>
2026-02-25 21:27:19 +1000

    Implement RAM scraping routine

--- src/main/java/com/price_tracker/webscraper/GPUScrapingService.java
+++ src/main/java/com/price_tracker/webscraper/GPUScrapingService.java
@@ -1,25 +1,29 @@
 package com.price_tracker.webscraper;
 
 import com.price_tracker.domain.entities.GPUPricePoint;
+import com.price_tracker.domain.entities.RAMPricePoint;
 import com.price_tracker.repositories.GPUPricePointRepository;
+import com.price_tracker.repositories.RAMPricePointRepository;
 import com.price_tracker.repositories.UmartProductRepository;
 import com.price_tracker.webscraper.product_services.impl.UmartGPUScraper;
+import com.price_tracker.webscraper.product_services.impl.UmartRAMScraper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
+import static com.price_tracker.constants.ScrapingConstants.SLEEPING_CONSTANT;
 
 @Service
 @RequiredArgsConstructor
 @Log
 public class GPUScrapingService {
 
-
-    private final Integer sleepingConstant = 1000; // for polite scraping
+    private final RAMPricePointRepository ramPricePointRepository;
     private final GPUPricePointRepository gpuPricePointRepository;
     private final UmartProductRepository umartProductRepository;
     private final UmartGPUScraper umartGPUScraper;
+    private final UmartRAMScraper umartRAMScraper;
 
     /** Core scraping service. Runs automatically at 3AM each day as per the CRON notation below. */
     @Scheduled(cron = "0 0 3 * * ?")
@@ -37,7 +41,7 @@ public class GPUScrapingService {
     public void runUmartGPUScrape() throws InterruptedException {
         for(String url : umartProductRepository.findUrlsForActiveGPUs()) {
             GPUPricePoint gpuPricePoint = scrapeGPU(url);
-            Thread.sleep(sleepingConstant);
+            Thread.sleep(SLEEPING_CONSTANT);
             if(gpuPricePoint != null) {
                 log.info("Got GPU Model: " + gpuPricePoint.getModelNumber() + " & GPU Price: "
                         + gpuPricePoint.getPrice());
@@ -50,6 +54,30 @@ public class GPUScrapingService {
         String[] scrapedData = umartGPUScraper.scrapeProductData(URL);
         if(umartGPUScraper.validateScrapedData(scrapedData))
             return umartGPUScraper.createGPUPricePoint(scrapedData);
+        else
+            return null;
+    }
+
+    public void runDailyRAMScrape() throws InterruptedException {
+        runUmartRAMScrape();
+    }
+
+    public void runUmartRAMScrape() throws InterruptedException {
+        for(String url : umartProductRepository.findUrlsForActiveRAM()) {
+            RAMPricePoint ramPricePoint = scrapeRAM(url);
+            Thread.sleep(SLEEPING_CONSTANT);
+            if(ramPricePoint != null) {
+                log.info("Got RAM Model: " + ramPricePoint.getModelNumber() + " & RAM  Price: "
+                        + ramPricePoint.getPrice());
+                ramPricePointRepository.save(ramPricePoint);
+            }
+        }
+    }
+
+    public RAMPricePoint scrapeRAM(String url) {
+        String[] scrapedData = umartRAMScraper.scrapeProductData(url);
+        if(umartRAMScraper.validateScrapedData(scrapedData))
+            return umartRAMScraper.createRAMPricePoint(scrapedData);
         else
             return null;
     }
