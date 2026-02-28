91254cc3a7378cbb44c39f96b8eb8bfa4d6060ea
jack-brady <jackbradyx@proton.me>
2026-02-28 18:30:23 +1000

    Add GPU & RAM testing constants

--- src/main/java/com/price_tracker/constants/TestingConstants.java
+++ src/main/java/com/price_tracker/constants/TestingConstants.java
@@ -2,6 +2,30 @@ package com.price_tracker.constants;
 
 public final class TestingConstants {
 
+    // gpu testing constants
     public static final String TESTING_GPU_MODEL_NUMBER = "PRIME-RTX5070TI-O16G";
+    public static final String TESTING_GPU_CHIP = "RTX 5070 Ti";
+    public static final String TESTING_GPU_CHIP_MANUFACTURER = "NVIDIA";
+    public static final String TESTING_GPU_BOARD_MANUFACTURER = "ASUS";
+    public static final String TESTING_GPU_NAME = "Asus Prime GeForce RTX 5070 Ti OC 16G Graphics Card";
+
+    // umart gpu product testing constants
+    public static final String TESTING_VENDOR_UMART = "UMART";
+    public static final String TESTING_GPU_PRICE = "1,499.00";
+    public static final String PRODUCT_TYPE_GPU = "GPU";
+
+    // ram testing constants
     public static final String TESTING_RAM_MODEL_NUMBER = "KF560C36BBE2K2-64";
+    public static final String TESTING_RAM_NAME = "Kingston 64GB (2x32GB) Fury Beast CL36 6000MHz DDR5 RAM (KF560C36BBE2K2-64)";
+    public static final String TESTING_RAM_BRAND = "KINGSTON";
+    public static final String TESTING_RAM_STANDARD = "DDR5";
+    public static final String TESTING_RAM_LATENCY = "CL36";
+    public static final Integer TESTING_RAM_VOLUME = 64;
+    public static final Integer TESTING_RAM_DIMM_COUNT = 2;
+    public static final Integer TESTING_RAM_CLOCKRATE = 6000;
+    public static final Double TESTING_RAM_VOLTAGE = 1.35;
+
+    // umart ram product testing constants
+    public static final String TESTING_RAM_PRICE = "1,299.00";
+    public static final String PRODUCT_TYPE_RAM = "RAM";
 }
