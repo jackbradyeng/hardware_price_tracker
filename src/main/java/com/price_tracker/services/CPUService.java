c85b0bc9734f1f6a49ab83c6e247822841d92191
jack-brady <jackbradyx@proton.me>
2026-03-03 19:08:13 +1000

    Add CPU service & implementation

---
+++ src/main/java/com/price_tracker/services/CPUService.java
@@ -0,0 +1,20 @@
+package com.price_tracker.services;
+
+import com.price_tracker.domain.entities.CPUEntity;
+import java.util.List;
+import java.util.Optional;
+
+public interface CPUService {
+
+    CPUEntity save(CPUEntity cpuEntity);
+
+    List<CPUEntity> findAll();
+
+    Optional<CPUEntity> findOne(String id);
+
+    boolean exists(String id);
+
+    CPUEntity partialUpdate(String id, CPUEntity cpuEntity);
+
+    void delete(String id);
+}
---
+++ src/main/java/com/price_tracker/services/impl/CPUServiceImpl.java
@@ -0,0 +1,53 @@
+package com.price_tracker.services.impl;
+
+import com.price_tracker.domain.entities.CPUEntity;
+import com.price_tracker.repositories.CPURepository;
+import com.price_tracker.services.CPUService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class CPUServiceImpl implements CPUService {
+
+    private final CPURepository cpuRepository;
+
+    @Override
+    public CPUEntity save(CPUEntity cpuEntity) {
+        return cpuRepository.save(cpuEntity);
+    }
+
+    @Override
+    public List<CPUEntity> findAll() {
+        return cpuRepository.findAll()
+                .stream()
+                .toList();
+    }
+
+    @Override
+    public Optional<CPUEntity> findOne(String id) {
+        return cpuRepository.findById(id);
+    }
+
+    @Override
+    public boolean exists(String id) {
+        return cpuRepository.existsById(id);
+    }
+
+    @Override
+    public CPUEntity partialUpdate(String id, CPUEntity cpuEntity) {
+        cpuEntity.setModelNumber(id);
+
+        return cpuRepository.findById(id).map(existingCPU -> {
+            Optional.ofNullable(cpuEntity.getName()).ifPresent(existingCPU::setName);
+            return cpuRepository.save(existingCPU);
+        }).orElseThrow(() -> new RuntimeException("CPU does not exist."));
+    }
+
+    @Override
+    public void delete(String id) {
+        cpuRepository.deleteById(id);
+    }
+}
