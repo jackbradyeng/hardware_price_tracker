7f95e4b7d1f18ad626f30254b5398e6d1e40a5a7
jack-brady <jackbradyx@proton.me>
2026-03-10 23:28:13 +1000

    Add sequence names

--- src/main/java/com/price_tracker/constants/DatabaseTableNames.java
+++ src/main/java/com/price_tracker/constants/DatabaseTableNames.java
@@ -10,4 +10,14 @@ public final class DatabaseTableNames {
     public static final String CPU_PRICE_HISTORY = "CPU_PRICE_HISTORY";
     public static final String VENDOR_TABLE_NAME = "VENDORS";
     public static final String UMART_PRODUCT_NAME = "UMART_PRODUCTS";
+
+    // sequence names
+    public static final String GPU_PRICE_SEQUENCE = "GPU_PRICE_SEQUENCE";
+    public static final String RAM_PRICE_SEQUENCE = "RAM_PRICE_SEQUENCE";
+    public static final String CPU_PRICE_SEQUENCE = "CPU_PRICE_SEQUENCE";
+
+    // generator names
+    public static final String GPU_PRICE_GEN = "GPU_PRICE_GEN";
+    public static final String RAM_PRICE_GEN = "RAM_PRICE_GEN";
+    public static final String CPU_PRICE_GEN = "CPU_PRICE_GEN";
 }
