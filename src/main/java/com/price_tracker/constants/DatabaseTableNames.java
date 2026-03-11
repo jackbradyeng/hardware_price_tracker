1544486e63be8da3343a0380a570c350b29418e8
jack-brady <jackbradyx@proton.me>
2026-03-11 23:07:01 +1000

    Update DB table & sequence names

--- src/main/java/com/price_tracker/constants/DatabaseTableNames.java
+++ src/main/java/com/price_tracker/constants/DatabaseTableNames.java
@@ -12,12 +12,12 @@ public final class DatabaseTableNames {
     public static final String UMART_PRODUCT_NAME = "UMART_PRODUCTS";
 
     // sequence names
-    public static final String GPU_PRICE_SEQUENCE = "GPU_PRICE_SEQUENCE";
-    public static final String RAM_PRICE_SEQUENCE = "RAM_PRICE_SEQUENCE";
-    public static final String CPU_PRICE_SEQUENCE = "CPU_PRICE_SEQUENCE";
+    public static final String GPU_PRICE_SEQUENCE = "gpu_price_sequence";
+    public static final String RAM_PRICE_SEQUENCE = "ram_price_sequence";
+    public static final String CPU_PRICE_SEQUENCE = "cpu_price_sequence";
 
     // generator names
-    public static final String GPU_PRICE_GEN = "GPU_PRICE_GEN";
-    public static final String RAM_PRICE_GEN = "RAM_PRICE_GEN";
-    public static final String CPU_PRICE_GEN = "CPU_PRICE_GEN";
+    public static final String GPU_PRICE_GEN = "gpu_price_gen";
+    public static final String RAM_PRICE_GEN = "ram_price_gen";
+    public static final String CPU_PRICE_GEN = "cpu_price_gen";
 }
