62d406ff349d9d61e249a35198d667f3e4737b36
jack-brady <jackbradyx@proton.me>
2026-03-16 15:20:30 +1000

    Add GPU workstation controller integration tests

--- src/main/java/com/price_tracker/constants/TestingConstants.java
+++ src/main/java/com/price_tracker/constants/TestingConstants.java
@@ -39,4 +39,16 @@ public final class TestingConstants {
     // umart ram product testing constants
     public static final String TESTING_RAM_PRICE = "1,299.00";
     public static final String PRODUCT_TYPE_RAM = "RAM";
+
+    // workstation gpu testing constants
+    public static final String TESTING_WS_GPU_MODEL_NUMBER = "900-5G144-2500-000";
+    public static final String TESTING_WS_GPU_NAME = "NVIDIA RTX Pro 6000 Blackwell Edition 96GB Workstation Graphics Card (900-5G144-2500-000)";
+    public static final String TESTING_WS_GPU_CHIP_MANUFACTURER = "NVIDIA";
+    public static final Integer TESTING_WS_GPU_MEMORY = 96;
+    public static final Integer TESTING_WS_GPU_MEMORY_INTERFACE = 512;
+    public static final Integer TESTING_WS_GPU_MEMORY_BANDWIDTH = 1792;
+    public static final Integer TESTING_WS_GPU_CUDA_CORES = 24064;
+    public static final Integer TESTING_WS_GPU_TENSOR_CORES = 752;
+    public static final Integer TESTING_WS_GPU_RT_CORES = 188;
+    public static final String TESTING_WS_GPU_SYS_INTERFACE = "PCI Express 5.0 x16";
 }
