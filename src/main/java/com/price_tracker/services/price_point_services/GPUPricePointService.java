71cce23e3e4db066d62f1b87da65303dfdfd2ca8
jack-brady <jackbradyx@proton.me>
2026-03-22 23:38:05 +1000

    Add GPU price point model number search to service

--- src/main/java/com/price_tracker/services/price_point_services/GPUPricePointService.java
+++ src/main/java/com/price_tracker/services/price_point_services/GPUPricePointService.java
@@ -1,5 +1,6 @@
 package com.price_tracker.services.price_point_services;
 
+import com.price_tracker.domain.dto.hybrid_dtos.GPUDataAndPricePointDTO;
 import com.price_tracker.domain.dto.price_point_dtos.GPUPricePointDTO;
 import java.util.List;
 import java.util.Optional;
@@ -7,6 +8,8 @@ import java.util.Optional;
 public interface GPUPricePointService {
 
     List<GPUPricePointDTO> findAll();
+
+    List<GPUDataAndPricePointDTO> findByModelNumber(String modelNumber);
 
     Optional<GPUPricePointDTO> findOne(Long id);
 }
--- src/main/java/com/price_tracker/services/price_point_services/impl/GPUPricePointServiceImpl.java
+++ src/main/java/com/price_tracker/services/price_point_services/impl/GPUPricePointServiceImpl.java
@@ -1,7 +1,9 @@
 package com.price_tracker.services.price_point_services.impl;
 
+import com.price_tracker.domain.dto.hybrid_dtos.GPUDataAndPricePointDTO;
 import com.price_tracker.domain.dto.price_point_dtos.GPUPricePointDTO;
 import com.price_tracker.mappers.price_point_mappers.GPUPricePointMapper;
+import com.price_tracker.mappers.product_mappers.GPUMapper;
 import com.price_tracker.repositories.price_point_repos.GPUPricePointRepository;
 import com.price_tracker.services.price_point_services.GPUPricePointService;
 import jakarta.transaction.Transactional;
@@ -17,12 +19,28 @@ public class GPUPricePointServiceImpl implements GPUPricePointService {
 
     private final GPUPricePointRepository gpuPricePointRepository;
     private final GPUPricePointMapper gpuPricePointMapper;
+    private final GPUMapper gpuMapper;
 
     @Override
     public List<GPUPricePointDTO> findAll() {
         return gpuPricePointRepository.findAll().stream()
                 .map(gpuPricePointMapper::mapTo)
                 .toList();
+    }
+
+    @Override
+    public List<GPUDataAndPricePointDTO> findByModelNumber(String modelNumber) {
+
+        // we need to map each of the constituent entity and price points to DTOs before exposing them in the API
+        return gpuPricePointRepository.getPricePointsByModelNumber(modelNumber).stream()
+                .map(projection -> GPUDataAndPricePointDTO.builder()
+                        .gpuPricePointDTO(
+                                gpuPricePointMapper.mapTo(projection.getGPUPricePoint())
+                        )
+                        .gpuDTO(
+                                gpuMapper.mapTo(projection.getGPUEntity()))
+                        .build()
+                ).toList();
     }
 
     @Override
