60c728c5a73f2e13005a620de39d23b6c008910f
jack-brady <jackbradyx@proton.me>
2026-03-22 23:43:11 +1000

    Add GPU price point model number endpoint

--- src/main/java/com/price_tracker/controllers/price_point_controllers/GPUPricePointController.java
+++ src/main/java/com/price_tracker/controllers/price_point_controllers/GPUPricePointController.java
@@ -1,5 +1,6 @@
 package com.price_tracker.controllers.price_point_controllers;
 
+import com.price_tracker.domain.dto.hybrid_dtos.GPUDataAndPricePointDTO;
 import com.price_tracker.domain.dto.price_point_dtos.GPUPricePointDTO;
 import com.price_tracker.services.price_point_services.GPUPricePointService;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.Optional;
@@ -23,6 +25,13 @@ public class GPUPricePointController {
     @GetMapping(path = "/api/gpu_pricepoints")
     public ResponseEntity<List<GPUPricePointDTO>> listGPUPricePoints() {
         return new ResponseEntity<>(gpuPricePointService.findAll(), HttpStatus.OK);
+    }
+
+    // gpu get price-points and gpu data by model number
+    @GetMapping(path = "/api/gpu_pricepoints/{modelNumber}")
+    public ResponseEntity<List<GPUDataAndPricePointDTO>> findGPUPricePointsByModelNumber(
+            @RequestParam String modelNumber) {
+        return new ResponseEntity<>(gpuPricePointService.findByModelNumber(modelNumber), HttpStatus.OK);
     }
 
     // gpu price point read-one endpoint
