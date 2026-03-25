17fb16f4a2bc1e5bab5b4b8b8c9fcdde9e07a5ce
jack-brady <jackbradyx@proton.me>
2026-03-25 23:11:57 +1000

    Add CPU price point model number endpoint

--- src/main/java/com/price_tracker/services/price_point_services/CPUPricePointService.java
+++ src/main/java/com/price_tracker/services/price_point_services/CPUPricePointService.java
@@ -3,7 +3,6 @@ package com.price_tracker.services.price_point_services;
 import com.price_tracker.domain.dto.hybrid_dtos.CPUDataAndPricePointDTO;
 import com.price_tracker.domain.dto.price_point_dtos.CPUPricePointDTO;
 import java.util.List;
-import java.util.Optional;
 
 public interface CPUPricePointService {
 
