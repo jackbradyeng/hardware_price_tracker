311fe8f3617a8b23d7f44950b68d340dd4565a79
jack-brady <jackbradyx@proton.me>
2026-03-25 23:11:12 +1000

    Add CPU price point model number search to service

--- src/main/java/com/price_tracker/services/price_point_services/CPUPricePointService.java
+++ src/main/java/com/price_tracker/services/price_point_services/CPUPricePointService.java
@@ -1,5 +1,6 @@
 package com.price_tracker.services.price_point_services;
 
+import com.price_tracker.domain.dto.hybrid_dtos.CPUDataAndPricePointDTO;
 import com.price_tracker.domain.dto.price_point_dtos.CPUPricePointDTO;
 import java.util.List;
 import java.util.Optional;
@@ -8,5 +9,5 @@ public interface CPUPricePointService {
 
     List<CPUPricePointDTO> findAll();
 
-    Optional<CPUPricePointDTO> findOne(Long id);
+    CPUDataAndPricePointDTO findByModelNumber(String modelNumber);
 }
--- src/main/java/com/price_tracker/services/price_point_services/impl/CPUPricePointServiceImpl.java
+++ src/main/java/com/price_tracker/services/price_point_services/impl/CPUPricePointServiceImpl.java
@@ -1,14 +1,18 @@
 package com.price_tracker.services.price_point_services.impl;
 
+import com.price_tracker.domain.dto.hybrid_dtos.CPUDataAndPricePointDTO;
+import com.price_tracker.domain.dto.hybrid_interfaces.CPUDataANdPricePointProjection;
 import com.price_tracker.domain.dto.price_point_dtos.CPUPricePointDTO;
+import com.price_tracker.domain.dto.product_dtos.CPUDTO;
+import com.price_tracker.domain.entities.product_entities.CPUEntity;
 import com.price_tracker.mappers.price_point_mappers.CPUPricePointMapper;
+import com.price_tracker.mappers.product_mappers.CPUMapper;
 import com.price_tracker.repositories.price_point_repos.CPUPricePointRepository;
 import com.price_tracker.services.price_point_services.CPUPricePointService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @Transactional
@@ -17,6 +21,7 @@ public class CPUPricePointServiceImpl implements CPUPricePointService {
 
     private final CPUPricePointRepository cpuPricePointRepository;
     private final CPUPricePointMapper cpuPricePointMapper;
+    private final CPUMapper cpuMapper;
 
     @Override
     public List<CPUPricePointDTO> findAll() {
@@ -26,8 +31,28 @@ public class CPUPricePointServiceImpl implements CPUPricePointService {
     }
 
     @Override
-    public Optional<CPUPricePointDTO> findOne(Long id) {
-        return cpuPricePointRepository.findById(id)
-                .map(cpuPricePointMapper::mapTo);
+    public CPUDataAndPricePointDTO findByModelNumber(String modelNumber) {
+
+        List<CPUDataANdPricePointProjection> resultList = cpuPricePointRepository
+                .getPricePointsByModelNumber(modelNumber);
+
+        // if list is empty return a 404
+        if (resultList.isEmpty()) {
+            return null;
+        }
+
+        // convert CPU to a DTO so we can expose it in our API
+        CPUEntity cpu = resultList.getFirst().getCPUEntity();
+        CPUDTO cpuDTO = cpuMapper.mapTo(cpu);
+
+        // convert CPU price points to a list of DTOs
+        List<CPUPricePointDTO> cpuPricePointDTOS = resultList.stream()
+                .map(result -> cpuPricePointMapper.mapTo(result.getCPUPricePoint()))
+                .toList();
+
+        return CPUDataAndPricePointDTO.builder()
+                .cpuDTO(cpuDTO)
+                .cpuPricePointDTOList(cpuPricePointDTOS)
+                .build();
     }
 }
