6c11ab6cf3eb828dea205f3f6795d06afec3cdb7
jack-brady <jackbradyx@proton.me>
2026-03-26 21:49:32 +1000

    Update test data utility

--- src/test/java/com/price_tracker/TestDataUtility.java
+++ src/test/java/com/price_tracker/TestDataUtility.java
@@ -1,6 +1,7 @@
 package com.price_tracker;
 
 import com.price_tracker.domain.dto.product_dtos.GPUDTO;
+import com.price_tracker.domain.dto.product_dtos.GPUWorkstationDTO;
 import com.price_tracker.domain.dto.product_dtos.RAMDTO;
 import com.price_tracker.domain.dto.vendor_dtos.UmartProductDTO;
 import com.price_tracker.domain.entities.product_entities.GPUEntity;
@@ -123,6 +124,22 @@ public class TestDataUtility {
 
     public GPUWorkstationEntity createTestWorkstationGPU() {
         return GPUWorkstationEntity.builder()
+                .modelNumber(TESTING_WS_GPU_MODEL_NUMBER)
+                .name(TESTING_WS_GPU_NAME)
+                .chipManufacturer(TESTING_WS_GPU_CHIP_MANUFACTURER)
+                .gpuMemory(TESTING_WS_GPU_MEMORY)
+                .memoryInterface(TESTING_WS_GPU_MEMORY_INTERFACE)
+                .memoryBandwidth(TESTING_WS_GPU_MEMORY_BANDWIDTH)
+                .cudaCores(TESTING_WS_GPU_CUDA_CORES)
+                .tensorCores(TESTING_WS_GPU_TENSOR_CORES)
+                .raytracingCores(TESTING_WS_GPU_RT_CORES)
+                .systemInterface(TESTING_WS_GPU_SYS_INTERFACE)
+                .isActive(true)
+                .build();
+    }
+
+    public GPUWorkstationDTO createTestWorkstationGPUDTO() {
+        return GPUWorkstationDTO.builder()
                 .modelNumber(TESTING_WS_GPU_MODEL_NUMBER)
                 .name(TESTING_WS_GPU_NAME)
                 .chipManufacturer(TESTING_WS_GPU_CHIP_MANUFACTURER)
