c0d6cab0420e6993ba20d754e5cae06f808a080a
jack-brady <jackbradyx@proton.me>
2026-03-26 21:08:28 +1000

    Add WS GPU price point model number search to service

--- src/main/java/com/price_tracker/services/price_point_services/impl/GPUWorkstationPricePointServiceImpl.java
+++ src/main/java/com/price_tracker/services/price_point_services/impl/GPUWorkstationPricePointServiceImpl.java
@@ -1,7 +1,12 @@
 package com.price_tracker.services.price_point_services.impl;
 
+import com.price_tracker.domain.dto.hybrid_dtos.GPUWorkstationDataAndPricePointDTO;
+import com.price_tracker.domain.dto.hybrid_interfaces.GPUWorkstationDataAndPricePointProjection;
 import com.price_tracker.domain.dto.price_point_dtos.GPUWorkstationPricePointDTO;
+import com.price_tracker.domain.dto.product_dtos.GPUWorkstationDTO;
+import com.price_tracker.domain.entities.product_entities.GPUWorkstationEntity;
 import com.price_tracker.mappers.price_point_mappers.GPUWorkstationPricePointMapper;
+import com.price_tracker.mappers.product_mappers.GPUWorkstationMapper;
 import com.price_tracker.repositories.price_point_repos.GPUWorkstationPricePointRepository;
 import com.price_tracker.services.price_point_services.GPUWorkstationPricePointService;
 import jakarta.transaction.Transactional;
@@ -17,6 +22,7 @@ public class GPUWorkstationPricePointServiceImpl implements GPUWorkstationPriceP
 
     private final GPUWorkstationPricePointRepository gpuWorkstationPricePointRepository;
     private final GPUWorkstationPricePointMapper gpuPricePointMapper;
+    private final GPUWorkstationMapper gpuWorkstationMapper;
 
     @Override
     public List<GPUWorkstationPricePointDTO> findAll() {
@@ -29,5 +35,31 @@ public class GPUWorkstationPricePointServiceImpl implements GPUWorkstationPriceP
     @Override
     public Optional<GPUWorkstationPricePointDTO> findOne(Long id) {
         return gpuWorkstationPricePointRepository.findById(id).map(gpuPricePointMapper::mapTo);
+    }
+
+    @Override
+    public GPUWorkstationDataAndPricePointDTO findByModelNumber(String modelNumber) {
+
+        List<GPUWorkstationDataAndPricePointProjection> resultList = gpuWorkstationPricePointRepository
+                .getPricePointsByModelNumber(modelNumber);
+
+        // if list is empty return a 404
+        if (resultList.isEmpty()) {
+            return null;
+        }
+
+        // convert GPUWorkstation to a DTO so we can expose it in our API
+        GPUWorkstationEntity gpuWorkstation = resultList.getFirst().getGPUWorkstationEntity();
+        GPUWorkstationDTO gpuWorkstationDTO = gpuWorkstationMapper.mapTo(gpuWorkstation);
+
+        // convert GPUWorkstation price points to a list of DTOs
+        List<GPUWorkstationPricePointDTO> gpuWorkstationPricePointDTOS = resultList.stream()
+                .map(result -> gpuPricePointMapper.mapTo(result.getGPUWorkstationPricePoint()))
+                .toList();
+
+        return GPUWorkstationDataAndPricePointDTO.builder()
+                .gpuWorkstationDTO(gpuWorkstationDTO)
+                .gpuWorkstationPricePointDTOList(gpuWorkstationPricePointDTOS)
+                .build();
     }
 }
