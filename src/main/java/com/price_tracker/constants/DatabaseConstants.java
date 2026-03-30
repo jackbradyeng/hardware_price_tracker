2e8ea4f0e2acc7b6e08427feffe26c16b85aed7b
jack-brady <jackbradyx@proton.me>
2026-03-30 21:24:22 +1000

    Remove hibernate sequence generator constants

--- src/main/java/com/price_tracker/constants/DatabaseConstants.java
+++ src/main/java/com/price_tracker/constants/DatabaseConstants.java
@@ -19,10 +19,4 @@ public final class DatabaseConstants {
     public static final String WS_GPU_PRICE_SEQUENCE = "ws_price_sequence";
     public static final String RAM_PRICE_SEQUENCE = "ram_price_sequence";
     public static final String CPU_PRICE_SEQUENCE = "cpu_price_sequence";
-
-    // generator names
-    public static final String GPU_PRICE_GEN = "gpu_price_gen";
-    public static final String WS_GPU_PRICE_GEN = "ws_gpu_price_gen";
-    public static final String RAM_PRICE_GEN = "ram_price_gen";
-    public static final String CPU_PRICE_GEN = "cpu_price_gen";
 }
