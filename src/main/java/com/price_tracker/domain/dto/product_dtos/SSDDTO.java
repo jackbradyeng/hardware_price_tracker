d7551c496812a7b29ccda38824979282d3796dfd
jack-brady <jackbradyx@proton.me>
2026-05-16 19:08:11 +1000

    Add SSD DTO

---
+++ src/main/java/com/price_tracker/domain/dto/product_dtos/SSDDTO.java
@@ -0,0 +1,22 @@
+package com.price_tracker.domain.dto.product_dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class SSDDTO {
+
+    private String modelNumber;
+    private String name;
+    private String brand;
+    private Integer capacity;
+    private Integer sequentialRead;
+    private Integer sequentialWrite;
+    private Long meanTimeBetweenFailures;
+    private String storageInterface;
+}
