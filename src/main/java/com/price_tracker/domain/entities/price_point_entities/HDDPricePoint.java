d39ac105bb5fb31e4eaa358794480b7769190d13
jack-brady <jackbradyx@proton.me>
2026-05-17 20:32:41 +1000

    Add HDD price point

---
+++ src/main/java/com/price_tracker/domain/entities/price_point_entities/HDDPricePoint.java
@@ -0,0 +1,28 @@
+package com.price_tracker.domain.entities.price_point_entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "HDD_PRICE_HISTORY")
+public class HDDPricePoint {
+
+    @Id
+    private Long id;
+    private String modelNumber;
+    private String vendor;
+    private String currency;
+    private BigDecimal price;
+    private LocalDateTime scrapedAt;
+}
