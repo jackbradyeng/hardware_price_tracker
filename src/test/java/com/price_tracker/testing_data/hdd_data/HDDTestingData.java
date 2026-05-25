0518d56b5fb0147943c8c557ea90b41896b11ca4
jack-brady <jackbradyx@proton.me>
2026-05-25 20:00:05 +1000

    Add storage testing data

---
+++ src/test/java/com/price_tracker/testing_data/hdd_data/HDDTestingData.java
@@ -0,0 +1,17 @@
+package com.price_tracker.testing_data.hdd_data;
+
+public class HDDTestingData {
+
+    public static final String TESTING_HDD_MODEL_NUMBER = "ST2000DM008";
+    public static final String TESTING_HDD_NAME = "Seagate BarraCuda 2TB Internal Hard Drive";
+    public static final String TESTING_HDD_BRAND = "SEAGATE";
+    public static final Integer TESTING_HDD_CAPACITY = 2000;
+    public static final Integer TESTING_HDD_SEQUENTIAL_READ = 190;
+    public static final Integer TESTING_HDD_SEQUENTIAL_WRITE = 190;
+    public static final Long TESTING_HDD_MTBF = 2000000L;
+    public static final String TESTING_HDD_STORAGE_INTERFACE = "SATA III";
+    public static final Float TESTING_HDD_FORM_FACTOR = 3.5f;
+
+    public static final String SECOND_TESTING_HDD_MODEL_NUMBER = "WD20EZAZ";
+    public static final String SECOND_TESTING_HDD_NAME = "WD Blue 2TB Desktop Hard Disk Drive";
+}
---
+++ src/test/java/com/price_tracker/testing_data/ssd_data/SSDTestingData.java
@@ -0,0 +1,16 @@
+package com.price_tracker.testing_data.ssd_data;
+
+public class SSDTestingData {
+
+    public static final String TESTING_SSD_MODEL_NUMBER = "MZ-77E500B/AM";
+    public static final String TESTING_SSD_NAME = "Samsung 860 EVO 500GB SATA III SSD";
+    public static final String TESTING_SSD_BRAND = "SAMSUNG";
+    public static final Integer TESTING_SSD_CAPACITY = 500;
+    public static final Integer TESTING_SSD_SEQUENTIAL_READ = 550;
+    public static final Integer TESTING_SSD_SEQUENTIAL_WRITE = 520;
+    public static final Long TESTING_SSD_MTBF = 1500000L;
+    public static final String TESTING_SSD_STORAGE_INTERFACE = "SATA III";
+
+    public static final String SECOND_TESTING_SSD_MODEL_NUMBER = "MZ-77E1T0B/AM";
+    public static final String SECOND_TESTING_SSD_NAME = "Samsung 860 EVO 1TB SATA III SSD";
+}
