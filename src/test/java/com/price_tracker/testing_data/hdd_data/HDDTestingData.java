c04da9881d4f172a069babc5b0986d2b537d31d7
jack-brady <jackbradyx@proton.me>
2026-05-29 17:02:41 +1000

    Update HDD testing data & utility

--- src/test/java/com/price_tracker/testing_data/hdd_data/HDDTestingData.java
+++ src/test/java/com/price_tracker/testing_data/hdd_data/HDDTestingData.java
@@ -3,13 +3,15 @@ package com.price_tracker.testing_data.hdd_data;
 public class HDDTestingData {
 
     // HDD ENTITY/DTO TESTING CONSTANTS
-    public static final String TESTING_HDD_MODEL_NUMBER = "ST2000DM008";
-    public static final String TESTING_HDD_NAME = "Seagate BarraCuda 2TB Internal Hard Drive";
+    public static final String TESTING_HDD_MODEL_NUMBER = "ST2000DM005";
+    public static final String TESTING_HDD_NAME = "Seagate 2TB Barracuda 5400RPM 256MB Cache 3.5in SATA Hard Drive (ST2000DM005)";
     public static final String TESTING_HDD_BRAND = "SEAGATE";
     public static final Integer TESTING_HDD_CAPACITY = 2000;
     public static final Integer TESTING_HDD_SEQUENTIAL_READ = 190;
     public static final Integer TESTING_HDD_SEQUENTIAL_WRITE = 190;
     public static final Long TESTING_HDD_MTBF = 2000000L;
+    public static final Integer TESTING_HDD_RPM = 5400;
+    public static final Integer TESTING_HDD_CACHE = 256;
     public static final String TESTING_HDD_STORAGE_INTERFACE = "SATA III";
     public static final Float TESTING_HDD_FORM_FACTOR = 3.5f;
     public static final String SECOND_TESTING_HDD_MODEL_NUMBER = "WD20EZAZ";
@@ -17,4 +19,5 @@ public class HDDTestingData {
 
     // UMART HDD PRODUCT TESTING CONSTANTS
     public static final String TESTING_HDD_PRICE = "189.00";
+    public static final String PRODUCT_TYPE_HDD = "HDD";
 }
--- src/test/java/com/price_tracker/testing_data/hdd_data/HDDTestingUtility.java
+++ src/test/java/com/price_tracker/testing_data/hdd_data/HDDTestingUtility.java
@@ -2,11 +2,16 @@ package com.price_tracker.testing_data.hdd_data;
 
 import com.price_tracker.domain.dto.product_dtos.HDDDTO;
 import com.price_tracker.domain.entities.product_entities.HDDEntity;
+import com.price_tracker.domain.entities.vendor_entities.UmartProductEntity;
 import com.price_tracker.mappers.product_mappers.HDDMapper;
+import com.price_tracker.webscraper.dtos.ScrapedDataDTO;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import static com.price_tracker.constants.vendor_constants.VendorNames.UMART;
+import static com.price_tracker.testing_data.vendor_data.UmartWebDomainNames.UMART_SEAGATE_ST2000DM005;
 import static com.price_tracker.testing_data.hdd_data.HDDTestingData.*;
 
 @Component
@@ -15,6 +20,7 @@ public class HDDTestingUtility {
 
     private final HDDMapper hddMapper;
 
+    /// SAMPLE ENTITIES/DTOS
     public HDDDTO createTestHDD() {
         return hddMapper.mapTo(HDDEntity.builder()
                 .modelNumber(TESTING_HDD_MODEL_NUMBER)
@@ -24,6 +30,8 @@ public class HDDTestingUtility {
                 .sequentialRead(TESTING_HDD_SEQUENTIAL_READ)
                 .sequentialWrite(TESTING_HDD_SEQUENTIAL_WRITE)
                 .meanTimeBetweenFailures(TESTING_HDD_MTBF)
+                .rpm(TESTING_HDD_RPM)
+                .cache(TESTING_HDD_CACHE)
                 .storageInterface(TESTING_HDD_STORAGE_INTERFACE)
                 .formFactor(TESTING_HDD_FORM_FACTOR)
                 .build());
@@ -38,6 +46,8 @@ public class HDDTestingUtility {
                 .sequentialRead(TESTING_HDD_SEQUENTIAL_READ)
                 .sequentialWrite(TESTING_HDD_SEQUENTIAL_WRITE)
                 .meanTimeBetweenFailures(TESTING_HDD_MTBF)
+                .rpm(TESTING_HDD_RPM)
+                .cache(TESTING_HDD_CACHE)
                 .storageInterface(TESTING_HDD_STORAGE_INTERFACE)
                 .formFactor(TESTING_HDD_FORM_FACTOR)
                 .build());
@@ -48,5 +58,23 @@ public class HDDTestingUtility {
         hddDTOs.add(createTestHDD());
         hddDTOs.add(createSecondTestHDD());
         return hddDTOs;
+    }
+
+    /// SAMPLE PRODUCTS
+    public UmartProductEntity createTestUmartHDD() {
+        return UmartProductEntity.builder()
+                .productType(PRODUCT_TYPE_HDD)
+                .modelNumber(TESTING_HDD_MODEL_NUMBER)
+                .vendor(UMART)
+                .url(UMART_SEAGATE_ST2000DM005)
+                .build();
+    }
+
+    /// SAMPLE PRICE POINTS
+    public ScrapedDataDTO createSampleHDDPricePointData() {
+        return ScrapedDataDTO.builder()
+                .modelNumber(TESTING_HDD_MODEL_NUMBER)
+                .price(new BigDecimal(TESTING_HDD_PRICE))
+                .build();
     }
 }
