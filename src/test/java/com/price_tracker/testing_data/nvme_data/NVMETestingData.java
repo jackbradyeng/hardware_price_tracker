9589a4666af56376a5a5f5717c0d42b8b4237749
jack-brady <jackbradyx@proton.me>
2026-05-29 17:03:02 +1000

    Update NVME testing data & utility

--- src/test/java/com/price_tracker/testing_data/nvme_data/NVMETestingData.java
+++ src/test/java/com/price_tracker/testing_data/nvme_data/NVMETestingData.java
@@ -3,18 +3,19 @@ package com.price_tracker.testing_data.nvme_data;
 public class NVMETestingData {
 
     // NVME ENTITY/DTO TESTING CONSTANTS
-    public static final String TESTING_NVME_MODEL_NUMBER = "MZ-V8P1T0B/AM";
-    public static final String TESTING_NVME_NAME = "Samsung 980 Pro 1TB NVMe SSD";
-    public static final String TESTING_NVME_BRAND = "SAMSUNG";
+    public static final String TESTING_NVME_MODEL_NUMBER = "CT1000P510SSD8";
+    public static final String TESTING_NVME_NAME = "Crucial P510 1TB PCIe Gen5 M.2 2280 NVMe SSD (CT1000P510SSD8)";
+    public static final String TESTING_NVME_BRAND = "CRUCIAL";
     public static final Integer TESTING_NVME_CAPACITY = 1000;
-    public static final Integer TESTING_NVME_SEQUENTIAL_READ = 7000;
-    public static final Integer TESTING_NVME_SEQUENTIAL_WRITE = 5000;
+    public static final Integer TESTING_NVME_SEQUENTIAL_READ = 11000;
+    public static final Integer TESTING_NVME_SEQUENTIAL_WRITE = 9500;
     public static final Long TESTING_NVME_MTBF = 1500000L;
-    public static final String TESTING_NVME_STORAGE_INTERFACE = "PCIe 4.0 x4";
+    public static final String TESTING_NVME_STORAGE_INTERFACE = "PCIe 5.0 x4";
     public static final Boolean TESTING_NVME_INCLUDES_HEAT_SINK = false;
     public static final String SECOND_TESTING_NVME_MODEL_NUMBER = "MZ-V8P2T0B/AM";
     public static final String SECOND_TESTING_NVME_NAME = "Samsung 980 Pro 2TB NVMe SSD";
 
     // UMART NVME PRODUCT TESTING CONSTANTS
-    public static final String TESTING_NVME_PRICE = "249.00";
+    public static final String TESTING_NVME_PRICE = "299.00";
+    public static final String PRODUCT_TYPE_NVME = "NVME";
 }
