a28d35b001d770527352ba1fa3ca2554c3526b2e
jack-brady-windows <jackbradyx@proton.me>
2026-07-02 16:26:29 +1000

    Apply composition to Umart workstation GPU scraping service

--- src/main/java/com/price_tracker/webscraper/orchestrators/GPUWorkstationScrapingOrchestrator.java
+++ src/main/java/com/price_tracker/webscraper/orchestrators/GPUWorkstationScrapingOrchestrator.java
@@ -51,6 +51,7 @@ public class GPUWorkstationScrapingOrchestrator {
         try {
             Thread.sleep(SLEEPING_CONSTANT);
             return umartGPUWorkstationScrapingService
+                    .getGenericVendorScraper()
                     .scrapeProductData(url, UMART_CSS_MODEL_LOCATION, UMART_CSS_PRICE_LOCATION)
                     .map(umartGPUWorkstationScrapingService::createGPUWorkstationPricePoint);
         } catch (InterruptedException e) {
--- src/test/java/com/price_tracker/scrapers/unit_tests/GPUWorkstationScraperUnitTests.java
+++ src/test/java/com/price_tracker/scrapers/unit_tests/GPUWorkstationScraperUnitTests.java
@@ -3,10 +3,10 @@ package com.price_tracker.scrapers.unit_tests;
 import com.price_tracker.webscraper.PricePointObserver;
 import com.price_tracker.webscraper.dtos.ScrapedDataDTO;
 import com.price_tracker.webscraper.product_services.impl.UmartGPUWorkstationScrapingService;
+import com.price_tracker.webscraper.vendor_templates.GenericUmartScraper;
 import org.junit.jupiter.api.Test;
 import java.math.BigDecimal;
 import java.util.Optional;
-
 import static com.price_tracker.constants.vendor_constants.VendorCSSLocations.UMART_CSS_MODEL_LOCATION;
 import static com.price_tracker.constants.vendor_constants.VendorCSSLocations.UMART_CSS_PRICE_LOCATION;
 import static com.price_tracker.testing_data.vendor_data.UmartWebDomainNames.UMART_RTX_PRO_6000;
@@ -14,31 +14,32 @@ import static com.price_tracker.testing_data.wsgpu_data.WorkstationGPUTestingDat
 
 public class GPUWorkstationScraperUnitTests {
 
-    private final UmartGPUWorkstationScrapingService scraper =
-            new UmartGPUWorkstationScrapingService(new PricePointObserver());
+    private final GenericUmartScraper genericUmartScraper = new GenericUmartScraper(new PricePointObserver());
+    private final UmartGPUWorkstationScrapingService vendorScraper = new UmartGPUWorkstationScrapingService(genericUmartScraper);
 
     @Test
     public void testThatUmartWSGPUScraperReturnsExpectedModelNumber() {
-        Optional<ScrapedDataDTO> scrapedDataDTO = scraper
+        Optional<ScrapedDataDTO> scrapedDataDTO = vendorScraper
+                .getGenericVendorScraper()
                 .scrapeProductData(UMART_RTX_PRO_6000, UMART_CSS_MODEL_LOCATION, UMART_CSS_PRICE_LOCATION);
         assert scrapedDataDTO.isPresent() && scrapedDataDTO.get().modelNumber().equals(TESTING_WS_GPU_MODEL_NUMBER);
     }
 
     @Test
     public void testThatUmartWSGPUScraperRemovesSemicolon() {
-        String refinedModelNumber = scraper.refineModelNumber("Model Number : " + TESTING_WS_GPU_MODEL_NUMBER);
+        String refinedModelNumber = genericUmartScraper.refineModelNumber("Model Number : " + TESTING_WS_GPU_MODEL_NUMBER);
         assert refinedModelNumber.equals(TESTING_WS_GPU_MODEL_NUMBER);
     }
 
     @Test
     public void testThatUmartWSGPUScraperRemovesSingleComma() {
-        BigDecimal refinedPrice = scraper.refinePrice("9,990.00");
+        BigDecimal refinedPrice = genericUmartScraper.refinePrice("9,990.00");
         assert refinedPrice.equals(new BigDecimal("9990.00"));
     }
 
     @Test
     public void testThatUmartWSGPUScraperRemovesMultipleCommas() {
-        BigDecimal refinedPrice = scraper.refinePrice("1,099,000.00");
+        BigDecimal refinedPrice = genericUmartScraper.refinePrice("1,099,000.00");
         assert refinedPrice.equals(new BigDecimal("1099000.00"));
     }
 }
