df8304dbb4a34bdbdebfb3c18dfcae35d6cae7d5
jack-brady-windows <jackbradyx@proton.me>
2026-07-04 22:15:34 +1000

    Wire generic vendor service & orchestrator interface to CPU scraping orchestrator

--- src/main/java/com/price_tracker/webscraper/orchestrators/umart_orchestrators/UmartCPUScrapingOrchestrator.java
+++ src/main/java/com/price_tracker/webscraper/orchestrators/umart_orchestrators/UmartCPUScrapingOrchestrator.java
@@ -1,30 +1,46 @@
 package com.price_tracker.webscraper.orchestrators.umart_orchestrators;
 
+import com.price_tracker.domain.dto.price_point_dtos.GenericPricePointDTO;
 import com.price_tracker.domain.entities.price_point_entities.CPUPricePoint;
+import com.price_tracker.mappers.GenericMapper;
+import com.price_tracker.mappers.MapperFactory;
 import com.price_tracker.repositories.price_point_repos.jdbc_templates.CPUPricePointJDBCTemplate;
 import com.price_tracker.repositories.vendor_repos.UmartProductRepository;
-import com.price_tracker.webscraper.product_services.impl.VendorCPUScrapingService;
-import lombok.RequiredArgsConstructor;
+import com.price_tracker.webscraper.orchestrators.GenericProductScrapingOrchestrator;
+import com.price_tracker.webscraper.product_services.impl.VendorProductScrapingService;
 import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
+import static com.price_tracker.constants.other_constants.CurrencyConstants.AUD;
 import static com.price_tracker.constants.other_constants.ScrapingConstants.UMART_CPU_SCRAPING_TIME;
-import static com.price_tracker.constants.other_constants.ScrapingConstants.SLEEPING_CONSTANT;
 import static com.price_tracker.constants.vendor_constants.VendorCSSLocations.UMART_CSS_MODEL_LOCATION;
 import static com.price_tracker.constants.vendor_constants.VendorCSSLocations.UMART_CSS_PRICE_LOCATION;
+import static com.price_tracker.constants.vendor_constants.VendorNames.UMART;
 
 @Log
 @Service
-@RequiredArgsConstructor
-public class UmartCPUScrapingOrchestrator {
+public class UmartCPUScrapingOrchestrator implements GenericProductScrapingOrchestrator {
 
     private final CPUPricePointJDBCTemplate cpuPricePointJDBCTemplate;
     private final UmartProductRepository umartProductRepository;
-    private final VendorCPUScrapingService vendorCPUScrapingService;
+    private final VendorProductScrapingService vendorProductScrapingService;
+    private final GenericMapper<CPUPricePoint, GenericPricePointDTO> pricePointMapper;
+
+    @Autowired
+    public UmartCPUScrapingOrchestrator(CPUPricePointJDBCTemplate cpuPricePointJDBCTemplate,
+                                        UmartProductRepository umartProductRepository,
+                                        VendorProductScrapingService vendorProductScrapingService,
+                                        MapperFactory mapperFactory) {
+        this.cpuPricePointJDBCTemplate = cpuPricePointJDBCTemplate;
+        this.umartProductRepository = umartProductRepository;
+        this.vendorProductScrapingService = vendorProductScrapingService;
+        this.pricePointMapper = mapperFactory.create(CPUPricePoint.class, GenericPricePointDTO.class);
+    }
 
     @Scheduled(cron = UMART_CPU_SCRAPING_TIME)
     public void runDailyScrape() {
@@ -36,28 +52,16 @@ public class UmartCPUScrapingOrchestrator {
 
         List<CPUPricePoint> pricePoints = umartProductRepository.findUrlsForActiveCPU()
                 .stream()
-                .map(this::processCPU)
+                .map(url -> processPricePoint(vendorProductScrapingService, url, UMART_CSS_MODEL_LOCATION,
+                        UMART_CSS_PRICE_LOCATION, UMART, AUD))
                 .flatMap(Optional::stream)
+                .map(pricePointMapper::mapFrom)
                 .toList();
 
         cpuPricePointJDBCTemplate.batchInsertPricePoints(pricePoints);
 
         Instant end = Instant.now();
         Duration timeElapsed = Duration.between(start, end);
-        log.info("CPU scraping service took %d seconds to execute.".formatted(timeElapsed.toSeconds()));
-    }
-
-    private Optional<CPUPricePoint> processCPU(String url) {
-        try {
-            Thread.sleep(SLEEPING_CONSTANT);
-            return vendorCPUScrapingService
-                    .getGenericVendorScraper()
-                    .scrapeProductData(url, UMART_CSS_MODEL_LOCATION, UMART_CSS_PRICE_LOCATION)
-                    .map(vendorCPUScrapingService::createCPUPricePoint);
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-            log.warning("Scraping interrupted for URL: " + url);
-            return Optional.empty();
-        }
+        log.info("%s CPU scraping service took %d seconds to execute.".formatted(UMART, timeElapsed.toSeconds()));
     }
 }
--- src/test/java/com/price_tracker/scrapers/unit_tests/CPUScraperUnitTests.java
+++ src/test/java/com/price_tracker/scrapers/unit_tests/CPUScraperUnitTests.java
@@ -2,7 +2,7 @@ package com.price_tracker.scrapers.unit_tests;
 
 import com.price_tracker.webscraper.PricePointObserver;
 import com.price_tracker.webscraper.dtos.ScrapedDataDTO;
-import com.price_tracker.webscraper.product_services.impl.VendorCPUScrapingService;
+import com.price_tracker.webscraper.product_services.impl.VendorProductScrapingService;
 import com.price_tracker.webscraper.vendor_templates.UmartProductScraper;
 import com.price_tracker.webscraper.vendor_templates.ScorptecProductScraper;
 import org.junit.jupiter.api.Test;
@@ -18,8 +18,8 @@ public class CPUScraperUnitTests {
     private final PricePointObserver pricePointObserver = new PricePointObserver();
     private final UmartProductScraper umartProductScraper = new UmartProductScraper(pricePointObserver);
     private final ScorptecProductScraper scorptecProductScraper = new ScorptecProductScraper(pricePointObserver);
-    private final VendorCPUScrapingService umartScraper = new VendorCPUScrapingService(umartProductScraper);
-    private final VendorCPUScrapingService scorptecScraper = new VendorCPUScrapingService(scorptecProductScraper);
+    private final VendorProductScrapingService umartScraper = new VendorProductScrapingService(umartProductScraper);
+    private final VendorProductScrapingService scorptecScraper = new VendorProductScrapingService(scorptecProductScraper);
 
     @Test
     public void testThatUmartCPUScrapingServiceReturnsExpectedModelNumber() {
