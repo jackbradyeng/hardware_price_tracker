676cb90c75b50400d642cf0e76554575c8b6f6f2
jack-brady-windows <jackbradyx@proton.me>
2026-07-06 18:54:43 +1000

    Wire generic data and price point projection to SSD PP repository

--- src/main/java/com/price_tracker/repositories/price_point_repos/SSDPricePointRepository.java
+++ src/main/java/com/price_tracker/repositories/price_point_repos/SSDPricePointRepository.java
@@ -1,7 +1,8 @@
 package com.price_tracker.repositories.price_point_repos;
 
-import com.price_tracker.domain.dto.hybrid_interfaces.SSDDataAndPricePointProjection;
+import com.price_tracker.domain.dto.hybrid_interfaces.GenericDataAndPricePointProjection;
 import com.price_tracker.domain.entities.price_point_entities.SSDPricePoint;
+import com.price_tracker.domain.entities.product_entities.SSDEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,10 +13,11 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface SSDPricePointRepository extends JpaRepository<SSDPricePoint, Long> {
 
-    @Query(value = "select p as SSDPricePoint, e as SSDEntity from SSDPricePoint p " +
+    @Query(value = "select p as pricePoint, e as entity from SSDPricePoint p " +
             "left join SSDEntity e on p.modelNumber = e.modelNumber " +
             "where p.modelNumber = :modelNumber " +
             "order by p.scrapedAt desc",
             countQuery = "SELECT COUNT(p) FROM SSDPricePoint p WHERE p.modelNumber = :modelNumber")
-    Page<SSDDataAndPricePointProjection> getPricePointsByModelNumber(@Param("modelNumber") String modelNumber, Pageable pageable);
+    Page<GenericDataAndPricePointProjection<SSDEntity, SSDPricePoint>> getPricePointsByModelNumber(
+            @Param("modelNumber") String modelNumber, Pageable pageable);
 }
