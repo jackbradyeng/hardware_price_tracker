34c62945b52b264e1de1b3e63abfe4e7df9d1b92
jack-brady-windows <jackbradyx@proton.me>
2026-07-06 21:07:10 +1000

    Add generic price point JDBC template

---
+++ src/main/java/com/price_tracker/repositories/price_point_repos/jdbc_templates/GenericPricePointJdbcTemplate.java
@@ -0,0 +1,55 @@
+package com.price_tracker.repositories.price_point_repos.jdbc_templates;
+
+import com.price_tracker.domain.entities.price_point_entities.GenericPricePoint;
+import jakarta.transaction.Transactional;
+import org.springframework.jdbc.core.JdbcTemplate;
+import java.sql.Timestamp;
+import java.util.List;
+
+/** A generic JDBC template for price point insertions. */
+public class GenericPricePointJdbcTemplate<T extends GenericPricePoint> {
+
+    private final JdbcTemplate jdbcTemplate;
+    private final String sequenceName;
+    private final String tableName;
+
+    public GenericPricePointJdbcTemplate(JdbcTemplate jdbcTemplate, String sequenceName, String tableName) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.sequenceName = sequenceName;
+        this.tableName = tableName;
+    }
+
+    // all or nothing - no partial-insertions allowed
+    @Transactional
+    public void batchInsertPricePoints(List<T> pricePoints) {
+
+        // fetch batch of IDs from the DB -> this avoids round-trips for each one
+        String idQuery = "SELECT NEXTVAL('" + sequenceName + "') FROM GENERATE_SERIES(1, ?)";
+        List<Long> ids = jdbcTemplate.queryForList(idQuery, Long.class, pricePoints.size());
+
+        // assign the IDs to each price point instance
+        for (int i = 0; i < pricePoints.size(); i++) {
+            pricePoints.get(i).setId(ids.get(i));
+        }
+
+        // execute the batch insert
+        String sql = "INSERT INTO " + tableName + " (id, model_number, vendor, currency, price, scraped_at) " +
+                "VALUES (?, ?, ?, ?, ?, ?)";
+
+        int batchSize = 50;
+
+        jdbcTemplate.batchUpdate(
+                sql,
+                pricePoints,
+                batchSize,
+                (ps, pricePoint) -> {
+                    ps.setLong(1, pricePoint.getId());
+                    ps.setString(2, pricePoint.getModelNumber());
+                    ps.setString(3, pricePoint.getVendor());
+                    ps.setString(4, pricePoint.getCurrency());
+                    ps.setBigDecimal(5, pricePoint.getPrice());
+                    ps.setTimestamp(6, Timestamp.valueOf(pricePoint.getScrapedAt()));
+                }
+        );
+    }
+}
