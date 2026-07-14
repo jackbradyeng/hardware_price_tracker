e8f599fa52781d5318e80c3e24db2b883d10f26c
jack-brady <jackbradyx@proton.me>
2026-07-15 09:55:57 +1000

    Add validated class-level annotation to product, price point, & vendor controllers

--- src/main/java/com/price_tracker/controllers/price_point_controllers/RAMPricePointController.java
+++ src/main/java/com/price_tracker/controllers/price_point_controllers/RAMPricePointController.java
@@ -10,11 +10,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Optional;
 
+@Validated
 @RestController
 @RequiredArgsConstructor
 public class RAMPricePointController {
