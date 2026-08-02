4fff7ea44763618a1009c79fae89af764a6e998b
jack-brady <jackbradyx@proton.me>
2026-08-02 18:40:08 +1000

    Add documentation to CORS web configuration

--- src/main/java/com/priceTracker/config/WebConfig.java
+++ src/main/java/com/priceTracker/config/WebConfig.java
@@ -8,7 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    // TypeScript-React-Vite front-end hosted on port 3000
+    /* TypeScript-React-Vite front-end hosted on port 3000. This is left over for local testing ONLY. On the web,
+    backend-to-frontend networking resolves over the Docker network. */
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/v*/**")
