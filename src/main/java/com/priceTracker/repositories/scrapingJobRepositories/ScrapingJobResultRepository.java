7bc36dc3a645b166beb909e3c0acb937e321019b
jack-brady <jackbradyx@proton.me>
2026-08-12 17:42:28 +1000

    Add generic scraping job result repository

---
+++ src/main/java/com/priceTracker/repositories/scrapingJobRepositories/ScrapingJobResultRepository.java
@@ -0,0 +1,9 @@
+package com.priceTracker.repositories.scrapingJobRepositories;
+
+import com.priceTracker.domain.entities.scrapingJobEntities.ScrapingJobResult;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ScrapingJobResultRepository extends JpaRepository<ScrapingJobResult, String> {
+}
