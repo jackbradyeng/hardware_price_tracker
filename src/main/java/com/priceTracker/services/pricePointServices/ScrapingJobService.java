9a48722f70c7dffcf9c83b11bbc81b2d5c1c74dc
jack-brady <jackbradyx@proton.me>
2026-08-26 19:15:18 +1000

    Wire mapper to scraping job service & have methods return DTO type

--- src/main/java/com/priceTracker/services/pricePointServices/ScrapingJobService.java
+++ src/main/java/com/priceTracker/services/pricePointServices/ScrapingJobService.java
@@ -1,22 +1,32 @@
 package com.priceTracker.services.pricePointServices;
 
+import com.priceTracker.domain.dto.scrapingJobDTOs.ScrapingJobResultDTO;
 import com.priceTracker.domain.entities.scrapingJobEntities.ScrapingJobResult;
+import com.priceTracker.mappers.GenericMapper;
+import com.priceTracker.mappers.MapperFactory;
 import com.priceTracker.repositories.scrapingJobRepositories.ScrapingJobResultRepository;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 @Service
-@RequiredArgsConstructor
 public class ScrapingJobService {
 
     private final ScrapingJobResultRepository scrapingJobResultRepository;
+    private final GenericMapper<ScrapingJobResult, ScrapingJobResultDTO> mapper;
+
+    @Autowired
+    public ScrapingJobService(ScrapingJobResultRepository scrapingJobResultRepository,
+                              MapperFactory mapperFactory) {
+        this.scrapingJobResultRepository = scrapingJobResultRepository;
+        this.mapper = mapperFactory.create(ScrapingJobResult.class, ScrapingJobResultDTO.class);
+    }
 
     // CREATE METHOD
-    public ScrapingJobResult saveResults(String vendor, String productType,
-                                         Integer recordsReceived, Integer recordsSaved) {
+    public ScrapingJobResultDTO saveResults(String vendor, String productType,
+                                            Integer recordsReceived, Integer recordsSaved) {
 
         ScrapingJobResult scrapingJobResult = ScrapingJobResult.builder()
                 .vendor(vendor)
@@ -26,23 +36,32 @@ public class ScrapingJobService {
                 .timeCompleted(LocalDateTime.now())
                 .build();
 
-        return scrapingJobResultRepository.save(scrapingJobResult);
+        ScrapingJobResult savedResult = scrapingJobResultRepository.save(scrapingJobResult);
+
+        return mapper.mapTo(savedResult);
     }
 
     // READ METHODS
-    public Optional<ScrapingJobResult> findResult(Long id) {
-        return scrapingJobResultRepository.findById(id);
+    public Optional<ScrapingJobResultDTO> findResult(Long id) {
+        return scrapingJobResultRepository.findById(id)
+                .map(mapper::mapTo);
     }
 
-    public List<ScrapingJobResult> findResults() {
-        return scrapingJobResultRepository.findAll();
+    public List<ScrapingJobResultDTO> findResults() {
+        return scrapingJobResultRepository.findAll().stream()
+                .map(mapper::mapTo)
+                .toList();
     }
 
-    public List<ScrapingJobResult> findResultsByProductType(String productType) {
-        return scrapingJobResultRepository.findScrapingJobsByProduct(productType);
+    public List<ScrapingJobResultDTO> findResultsByProductType(String productType) {
+        return scrapingJobResultRepository.findScrapingJobsByProduct(productType).stream()
+                .map(mapper::mapTo)
+                .toList();
     }
 
-    public List<ScrapingJobResult> findResultsByVendor(String vendor) {
-        return scrapingJobResultRepository.findScrapingJobsByVendor(vendor);
+    public List<ScrapingJobResultDTO> findResultsByVendor(String vendor) {
+        return scrapingJobResultRepository.findScrapingJobsByVendor(vendor).stream()
+                .map(mapper::mapTo)
+                .toList();
     }
 }
