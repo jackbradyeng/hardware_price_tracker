c864ed85babca81e888e2b943dafa567ce605f8d
jack-brady <jackbradyx@proton.me>
2026-08-29 16:29:03 +1000

    Add scraping job testing data & utility

---
+++ src/test/java/com/priceTracker/testingData/scrapingJobData/ScrapingJobTestingData.java
@@ -0,0 +1,15 @@
+package com.priceTracker.testingData.scrapingJobData;
+
+import static com.priceTracker.constants.VendorNames.SCORPTEC;
+import static com.priceTracker.constants.VendorNames.UMART;
+
+public class ScrapingJobTestingData {
+
+    // SCRAPING JOB RESULT TESTING CONSTANTS
+    public static final String TESTING_SCRAPING_JOB_VENDOR = UMART;
+    public static final String TESTING_SCRAPING_JOB_PRODUCT_TYPE = "GPU";
+    public static final String TESTING_SCRAPING_JOB_OTHER_VENDOR = SCORPTEC;
+    public static final String TESTING_SCRAPING_JOB_OTHER_PRODUCT_TYPE = "CPU";
+    public static final Integer TESTING_SCRAPING_JOB_RECORDS_RECEIVED = 10;
+    public static final Integer TESTING_SCRAPING_JOB_RECORDS_SAVED = 8;
+}
